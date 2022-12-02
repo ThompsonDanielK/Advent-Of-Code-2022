b75a9602fcff42f708ada13aab64345547952202
Dan Thompson <thompsondanielk@gmail.com>
2022-12-01 20:53:06 -0500

    Added puzzle solving logic

--- AdventOfCode2022/PuzzleSolvers/Puzzle1.cs
+++ AdventOfCode2022/PuzzleSolvers/Puzzle1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdventOfCode2022.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,26 @@ namespace AdventOfCode2022.PuzzleSolvers
     {
         public static int Solve(List<string> puzzleInput)
         {
-            return 0;
+            int highestTotal = 0;
+            int loopTotal = 0;
+
+            for (int i = 0; i < puzzleInput.Count(); i++)
+            {
+                string line = puzzleInput[i];
+                if (line == string.Empty || i == puzzleInput.Count() - 1)
+                {
+                    if (loopTotal > highestTotal)
+                    {
+
+                        highestTotal = loopTotal;
+                    }
+                    loopTotal = 0;
+                }
+                int number = InputValidator.ParseStringToInt(line);
+                loopTotal += number;
+            }
+
+            return highestTotal;
         }
     }
 }
