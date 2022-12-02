cb765850cc41f1779790fc4277d07a76ba72f2f2
Dan Thompson <thompsondanielk@gmail.com>
2022-12-01 20:52:39 -0500

    Added utility to retrieve message

---
+++ AdventOfCode2022/Utils/DirectoryInformation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Utils
+{
+    public static class DirectoryInformation
+    {
+        /// <summary>
+        /// Retrives file path. I totally stole this from stack overflow.
+        /// </summary>
+        /// <param name="currentPath"></param>
+        /// <returns></returns>
+        public static DirectoryInfo TryGetSolutionDirectoryInfo(string currentPath = null)
+        {
+            var directory = new DirectoryInfo(
+                currentPath ?? Directory.GetCurrentDirectory());
+            while (directory != null && !directory.GetFiles("*.sln").Any())
+            {
+                directory = directory.Parent;
+            }
+            return directory;
+        }
+    }
+}
